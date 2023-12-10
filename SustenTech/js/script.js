5259b7c9d412f306a8a540d34d4cc83984ed6fd1
Caio <Caiokaikai@gmail.com>
2023-12-09 22:14:09 -0300

    adicionando metodos em javascript

--- SustenTech/js/script.js
+++ SustenTech/js/script.js
@@ -8,3 +8,31 @@ elementosDuvida.forEach(function (duvida)
         duvida.classList.toggle('ativa')
     })
 })
+
+var Botao = document.querySelector('.botao');
+
+Botao.addEventListener('mouseover', function() 
+{
+    this.style.backgroundColor = "red";
+});
+
+Botao.addEventListener('mouseout', function() 
+{
+    this.style.backgroundColor = "#009989";
+});
+
+// var Header = document.querySelector('.header');
+
+// window.addEventListener('scroll', function() {
+//     if (window.scrollY > 0) 
+//     {
+//         Header.classList.remove('.header'); // Verde quando está no topo
+//         Header.classList.add('.header.scrolled'); // Verde quando está no topo
+//     } else 
+//     {
+
+//         Header.style.backgroundColor = ".header"; // Vermelho quando está no topo
+//     }
+// });
+
+
